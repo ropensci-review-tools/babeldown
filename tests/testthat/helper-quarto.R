3fdabd47e2102a19380f3226a37e3a3a85596787
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-02-22 13:34:20 +0100

    tests: Add function creating a Quarto example book with files in two languages Fix #3

---
+++ tests/testthat/helper-quarto.R
@@ -0,0 +1,24 @@
+quarto_bilingual_book <- function(further_languages = c("es", "fr"),
+                                  dir,
+                                  subdir) {
+
+  withr::local_dir(dir)
+  quarto_bin <- quarto::quarto_path()
+  sys::exec_wait(
+    quarto_bin,
+    args = c("create-project", subdir, "--type", "book")
+  )
+
+  qmds <- dir(subdir, pattern = "\\.qmd", full.names = TRUE)
+
+  create_new_lang_file <- function(qmd_file, language) {
+    new_path <- fs::path_ext_set(qmd_file, sprintf(".%s.qmd", language))
+    fs::file_copy(qmd_file, new_path)
+  }
+
+  purrr::walk(
+    further_languages,
+    ~ purrr::walk(qmds, create_new_lang_file, language = .x)
+  )
+
+}
