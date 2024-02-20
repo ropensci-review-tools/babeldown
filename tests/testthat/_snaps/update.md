a6c076ba862392dd48d06675741786daca935158
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-02-20 12:10:06 +0100

    split test cases

--- tests/testthat/_snaps/update.md
+++ tests/testthat/_snaps/update.md
@@ -4,6 +4,17 @@
       gert::git_ls(repo = dir)[, 1]
     Output
       # A tibble: 2 x 1
+        path     
+        <chr>    
+      1 bla.es.md
+      2 bla.md   
+
+# deepl_update() works -- files in subdirectory
+
+    Code
+      gert::git_ls(repo = dir)[, 1]
+    Output
+      # A tibble: 2 x 1
         path         
         <chr>        
       1 pof/bla.es.md
--- tests/testthat/test-update.R
+++ tests/testthat/test-update.R
@@ -1,6 +1,67 @@
 test_that("deepl_update() works", {
 
   dir <- withr::local_tempdir()
+  file <- file.path(dir, "bla.md")
+  fs::file_create(file)
+  out_file <- file.path(dir, "bla.es.md")
+
+  brio::write_lines(
+    c("# header", "", "this is some text", "## subtitle", "", "nice!"),
+    file
+  )
+
+  with_mock_dir("git1", {
+    deepl_translate(
+      path = file,
+      out_path = out_file,
+      source_lang = "EN",
+      target_lang = "ES",
+      formality = "less",
+      yaml_fields = NULL
+    )
+  })
+
+  gert::git_init(dir)
+  gert::git_config_set("user.name", "Jane Doe", repo = dir)
+  gert::git_config_set("user.email", "jane@example.com", repo = dir)
+  gert::git_add(c(fs::path_rel(file, dir), fs::path_rel(out_file, dir)), repo = dir)
+  gert::git_commit_all("First commit", repo = dir)
+
+  brio::write_lines(
+    c("# a title", "", "this is some text", "", "awesome", "", "## subtitle", ""),
+    file
+  )
+  gert::git_add(fs::path_rel(file, dir), repo = dir)
+  gert::git_commit("Second commit", repo = dir)
+
+  original_translation <- brio::read_lines(out_file)
+
+  expect_snapshot(gert::git_ls(repo = dir)[,1])
+
+  with_mock_dir("git2", {
+    deepl_update(
+      path = file,
+      out_path = out_file,
+      source_lang = "EN",
+      target_lang = "ES",
+      formality = "less",
+      yaml_fields = NULL
+    )
+  })
+
+  new_translation <- brio::read_lines(out_file)
+  expect_true(
+    !all(new_translation %in% original_translation)
+  )
+  expect_true(
+    any(new_translation %in% original_translation)
+  )
+
+})
+
+test_that("deepl_update() works -- files in subdirectory", {
+
+  dir <- withr::local_tempdir()
   fs::dir_create(file.path(dir, "pof"))
   file <- file.path(dir, "pof", "bla.md")
   fs::file_create(file)
